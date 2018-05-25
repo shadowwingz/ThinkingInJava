f760ff6ae848273336a4753df721870b03b5c8b0
shadowwingz <shadowwingzhang@gmail.com>
2018-05-25 23:17:05 +0800

    确保正确清理

--- src/chapter7/CADSystem.java
+++ src/chapter7/CADSystem.java
@@ -2,8 +2,10 @@ package chapter7;
 
 /**
  * Created by shadowwingz on 2018/5/9 0009.
+ *
+ * 调用构造器时，从基类开始调用，扩散到子类
+ * 调用清理方法时，从子类开始调用，扩散到基类
  */
-
 class Shape {
     public Shape(int i) {
         System.out.println("Shape constructor");
@@ -94,3 +96,33 @@ public class CADSystem extends Shape {
         }
     }
 }
+
+/*
+输出：
+
+Shape constructor
+Shape constructor
+Drawing line : 0. 0
+Shape constructor
+Drawing line : 1. 1
+Shape constructor
+Drawing line : 2. 4
+Shape constructor
+Drawing Circle
+Shape constructor
+Drawing Triangle
+Combined constructor
+CADSystem dispose
+Erasing Triangle
+Shape dispose
+Erasing Circle
+Shape dispose
+Erasing line : 2. 4
+Shape dispose
+Erasing line : 1. 1
+Shape dispose
+Erasing line : 0. 0
+Shape dispose
+Shape dispose
+
+ */
