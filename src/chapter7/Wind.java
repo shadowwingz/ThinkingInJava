466ae9b4e5b808ee996949a0bfdf6edbd77d670d
shadowwingz <shadowwingzhang@gmail.com>
2018-05-30 13:50:33 +0800

    向上转型的一些细节

--- src/chapter7/Wind.java
+++ src/chapter7/Wind.java
@@ -2,11 +2,28 @@ package chapter7;
 
 /**
  * Created by shadowwingz on 2018/5/27 0027.
+ *
+ * 向上转型时，
+ * 无法调用子类特有的方法，
+ * 可以调用父类特有的方法
+ * 如果调用父类和子类都有的方法，则实际上会调用子类的方法
+ *
+ * 相关问题：java继承中，为什么向上转型后无法运行子类中特有的方法？
+ * https://segmentfault.com/q/1010000004558570
+ *
+ * 摘要：
+ *
+ * 是变量的声明类型决定了它能调用哪些方法，而不是它指向的堆中的对象的实际类型。
+ * 
  */
 // 乐器
 class Instrument {
 
     public void play() {
+        System.out.println("playInstrument");
+    }
+
+    public void playInstrument() {
     }
 
     // 调试
@@ -24,6 +41,30 @@ public class Wind extends Instrument {
          * 这里我们传入 Instrument 的子类，也就是 Wind，
          * 编译器会自动把 Wind 转换为 Instrument（向上转型）
          */
-        Instrument.tune(flute);
+//        Instrument.tune(flute);
+
+        Instrument instrument = new Wind();
+        /**
+         * 调用父类和子类都有的方法，实际上调用的是子类的方法
+         *
+         * 输出：playWind
+         */
+        instrument.play();
+        /**
+         * 可以调用
+         */
+        instrument.playInstrument();
+
+        /**
+         * 无法调用子类特有的方法，也就是说，向上转型会丢失方法
+         */
+//        instrument.playWind();
+    }
+
+    public void play() {
+        System.out.println("playWind");
+    }
+
+    public void playWind() {
     }
 }
