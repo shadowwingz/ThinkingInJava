6ab287e5d255b0d1e56f51c182523a02fc6a5567
shadowwingz <shadowwingzhang@gmail.com>
2018-06-09 18:06:49 +0800

    接口中的域

---
+++ src/chapter9/Months.java
@@ -0,0 +1,22 @@
+package chapter9;
+
+/**
+ * Created by shadowwingz on 2018/6/9 0009.
+ * <p>
+ * 接口中的变量都自动是 static 和 final 的。
+ */
+public interface Months {
+    int
+            JANUARY = 1,
+            FEBRUARY = 2,
+            MARCH = 3,
+            APRIL = 4,
+            MAY = 5,
+            JUNE = 6,
+            JULY = 7,
+            AUGUST = 8,
+            SEPTEMBER = 9,
+            OCTOBER = 10,
+            NOVEMBER = 11,
+            DECEMBER = 12;
+}
