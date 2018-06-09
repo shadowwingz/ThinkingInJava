5d91ab055d7df183c2fcac87583c1498ae042e07
shadowwingz <shadowwingzhang@gmail.com>
2018-06-09 18:06:14 +0800

    适配接口补充

---
+++ src/chapter9/RandomDoubles.java
@@ -0,0 +1,35 @@
+package chapter9;
+
+import java.util.Random;
+
+/**
+ * Created by shadowwingz on 2018/6/9 0009.
+ */
+public class RandomDoubles {
+
+    private static Random rand = new Random(47);
+
+    public double next() {
+        return rand.nextDouble();
+    }
+
+    public static void main(String[] args) {
+        RandomDoubles rd = new RandomDoubles();
+        for (int i = 0; i < 7; i++) {
+            System.out.println(rd.next());
+        }
+    }
+}
+
+/*
+输出：
+
+0.7271157860730044
+0.5309454508634242
+0.16020656493302599
+0.18847866977771732
+0.5166020801268457
+0.2678662084200585
+0.2613610344283964
+
+ */
---
+++ src/chapter9/RandomWords.java
@@ -0,0 +1,67 @@
+package chapter9;
+
+import java.io.IOException;
+import java.nio.CharBuffer;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * Created by shadowwingz on 2018/6/9 0009.
+ */
+public class RandomWords implements Readable {
+
+    private static Random rand = new Random(47);
+
+    private static final char[] capitals =
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+
+    private static final char[] lowers =
+            "abcdefghijklmnopqrstuvwxyz".toCharArray();
+
+    private static final char[] vowels = "aeios".toCharArray();
+
+    private int count;
+
+    public RandomWords(int count) {
+        this.count = count;
+    }
+
+    @Override
+    public int read(CharBuffer cb) throws IOException {
+        if (count-- == 0)
+            return -1;
+
+        cb.append(capitals[rand.nextInt(capitals.length)]);
+
+        for (int i = 0; i < 4; i++) {
+            cb.append(vowels[rand.nextInt(vowels.length)]);
+            cb.append(lowers[rand.nextInt(lowers.length)]);
+        }
+
+        cb.append(" ");
+        return 10;
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(new RandomWords(10));
+        while (s.hasNext()) {
+            System.out.println(s.next());
+        }
+    }
+}
+
+/*
+输出：
+
+Yazersyac
+Fowenscor
+Goeazimom
+Raesuacio
+Nsoadesiw
+Hageaiksx
+Rsqicibsi
+Nsmasetih
+Ksusuozog
+Waqizeyoy
+
+ */
