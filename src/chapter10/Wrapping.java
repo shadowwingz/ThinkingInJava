b45c1f6e93008c8942371c5ad2134f8f8a79809a
shadowwingz <shadowwingzhang@gmail.com>
2018-06-11 23:34:25 +0800

    匿名内部类的基类

---
+++ src/chapter10/Wrapping.java
@@ -0,0 +1,17 @@
+package chapter10;
+
+/**
+ * Created by shadowwingz on 2018/6/11 0011.
+ */
+public class Wrapping {
+
+    private int i;
+
+    public Wrapping(int x) {
+        i = x;
+    }
+
+    public int value() {
+        return i;
+    }
+}
