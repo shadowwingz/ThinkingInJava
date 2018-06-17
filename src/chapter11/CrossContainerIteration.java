bb546e5437620e0cf18e9e75952f9bc624d35328
shadowwingz <shadowwingzhang@gmail.com>
2018-06-17 22:09:29 +0800

    迭代器

---
+++ src/chapter11/CrossContainerIteration.java
@@ -0,0 +1,43 @@
+package chapter11;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.TreeSet;
+
+import typeinfo.pets.Pet;
+import typeinfo.pets.Pets;
+
+/**
+ * Created by shadowwingz on 2018/6/17 0017.
+ */
+public class CrossContainerIteration {
+
+    public static void display(Iterator<Pet> it) {
+        while (it.hasNext()) {
+            Pet p = it.next();
+            System.out.print(p.id() + ":" + p + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Pet> pets = Pets.arrayList(8);
+        LinkedList<Pet> petsLL = new LinkedList<>(pets);
+        HashSet<Pet> petsHS = new HashSet<>(pets);
+        TreeSet<Pet> petsTS = new TreeSet<>(pets);
+        display(pets.iterator());
+        display(petsLL.iterator());
+        display(petsHS.iterator());
+        display(petsTS.iterator());
+    }
+}
+
+/*
+输出：
+
+0:Rat 1:Manx 2:Cymric 3:Mutt 4:Pug 5:Cymric 6:Pug 7:Manx 8:Cymric 9:Rat 10:EgyptianMau 11:Hamster
+0:Rat 1:Manx 2:Cymric 3:Mutt 4:Pug 5:Cymric 6:Pug 7:Manx 8:Cymric 9:Rat 10:EgyptianMau 11:Hamster
+[Pug, Manx, Cymric, Rat, EgyptianMau, Hamster]
+
+ */
---
+++ src/chapter11/SimpleIteration.java
@@ -0,0 +1,41 @@
+package chapter11;
+
+import java.util.Iterator;
+import java.util.List;
+
+import typeinfo.pets.Pet;
+import typeinfo.pets.Pets;
+
+/**
+ * Created by shadowwingz on 2018/6/17 0017.
+ */
+public class SimpleIteration {
+    public static void main(String[] args) {
+        List<Pet> pets = Pets.arrayList(12);
+        Iterator<Pet> it = pets.iterator();
+        while (it.hasNext()) {
+            Pet p = it.next();
+            System.out.print(p.id() + ":" + p + " ");
+        }
+        System.out.println();
+        for (Pet p : pets) {
+            System.out.print(p.id() + ":" + p + " ");
+        }
+        System.out.println();
+        it = pets.iterator();
+        for (int i = 0; i < 6; i++) {
+            it.next();
+            it.remove();
+        }
+        System.out.println(pets);
+    }
+}
+
+/*
+输出：
+
+0:Rat 1:Manx 2:Cymric 3:Mutt 4:Pug 5:Cymric 6:Pug 7:Manx 8:Cymric 9:Rat 10:EgyptianMau 11:Hamster
+0:Rat 1:Manx 2:Cymric 3:Mutt 4:Pug 5:Cymric 6:Pug 7:Manx 8:Cymric 9:Rat 10:EgyptianMau 11:Hamster
+[Pug, Manx, Cymric, Rat, EgyptianMau, Hamster]
+
+ */
