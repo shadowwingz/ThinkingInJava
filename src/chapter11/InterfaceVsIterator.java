80609b0ca47ec120917c879eb32250f05f26a08b
shadowwingz <shadowwingzhang@gmail.com>
2018-06-18 22:15:19 +0800

    Collection 和 Iterator

---
+++ src/chapter11/InterfaceVsIterator.java
@@ -0,0 +1,66 @@
+package chapter11;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import typeinfo.pets.Pet;
+import typeinfo.pets.Pets;
+
+/**
+ * Created by shadowwingz on 2018-06-18 21:51
+ *
+ * Collection 和 Iterator
+ */
+public class InterfaceVsIterator {
+
+    public static void display(Iterator<Pet> it) {
+        while (it.hasNext()) {
+            Pet p = it.next();
+            System.out.print(p.id() + ":" + p + " ");
+        }
+        System.out.println();
+    }
+
+    public static void display(Collection<Pet> pets) {
+        for (Pet p : pets) {
+            System.out.print(p.id() + ":" + p + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        List<Pet> petList = Pets.arrayList(8);
+        Set<Pet> petSet = new HashSet<>(petList);
+        Map<String, Pet> petMap = new LinkedHashMap<>();
+        String[] names = ("Ralph, Eric, Robin, Lacey, " +
+                "Britney, Sam, Spot, Fluffy").split(", ");
+        for (int i = 0; i < names.length; i++) {
+            petMap.put(names[i], petList.get(i));
+        }
+        display(petList);
+        display(petSet);
+        display(petList.iterator());
+        display(petSet.iterator());
+        System.out.println(petMap);
+        display(petMap.values());
+        display(petMap.values().iterator());
+    }
+}
+
+/*
+输出：
+
+0:Rat 1:Manx 2:Cymric 3:Mutt 4:Pug 5:Cymric 6:Pug 7:Manx
+0:Rat 1:Manx 2:Cymric 3:Mutt 4:Pug 5:Cymric 6:Pug 7:Manx
+0:Rat 1:Manx 2:Cymric 3:Mutt 4:Pug 5:Cymric 6:Pug 7:Manx
+0:Rat 1:Manx 2:Cymric 3:Mutt 4:Pug 5:Cymric 6:Pug 7:Manx
+{Ralph=Rat, Eric=Manx, Robin=Cymric, Lacey=Mutt, Britney=Pug, Sam=Cymric, Spot=Pug, Fluffy=Manx}
+0:Rat 1:Manx 2:Cymric 3:Mutt 4:Pug 5:Cymric 6:Pug 7:Manx
+0:Rat 1:Manx 2:Cymric 3:Mutt 4:Pug 5:Cymric 6:Pug 7:Manx
+
+ */
