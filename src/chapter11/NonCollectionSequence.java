1a80a6638d92ebe6896b62c4c01b081d500228b3
shadowwingz <shadowwingzhang@gmail.com>
2018-06-18 22:47:48 +0800

    Collection 和 Iterator 补充

--- src/chapter11/NonCollectionSequence.java
+++ src/chapter11/NonCollectionSequence.java
@@ -36,3 +36,10 @@ public class NonCollectionSequence extends PetSequence {
         InterfaceVsIterator.display(nc.iterator());
     }
 }
+
+/*
+输出：
+
+0:Rat 1:Manx 2:Cymric 3:Mutt 4:Pug 5:Cymric 6:Pug 7:Manx
+
+ */
