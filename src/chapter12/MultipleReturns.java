6843dce9500133337d4cd8a88296cc42fbda4d4a
shadowwingz <shadowwingzhang@gmail.com>
2018-06-23 16:34:36 +0800

    在 return 中使用 finally

---
+++ src/chapter12/MultipleReturns.java
@@ -0,0 +1,59 @@
+package chapter12;
+
+/**
+ * Created by shadowwingz on 2018-06-23 16:10
+ *
+ * 在 try 语句中，调用 return，会执行 finally 语句。
+ */
+public class MultipleReturns {
+
+    public static void f(int i) {
+        System.out.println("Initialization that requires cleanup");
+        try {
+            System.out.println("Point 1");
+            if (i == 1)
+                return;
+            System.out.println("Point 2");
+            if (i == 2)
+                return;
+            System.out.println("Point 3");
+            if (i == 3)
+                return;
+            System.out.println("End");
+            return;
+        } finally {
+            System.out.println("Performing cleanup");
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+            f(i);
+            System.out.println();
+        }
+    }
+}
+
+/*
+输出：
+
+Initialization that requires cleanup
+Point 1
+Point 2
+Point 3
+End
+Performing cleanup
+Initialization that requires cleanup
+Point 1
+Performing cleanup
+Initialization that requires cleanup
+Point 1
+Point 2
+Performing cleanup
+Initialization that requires cleanup
+Point 1
+Point 2
+Point 3
+Performing cleanup
+
+ */
