3722f92b2a478dcc406f99a3a0801b58e3b2df10
shadowwingz <shadowwingzhang@gmail.com>
2018-07-24 20:07:51 +0800

    从任务中产生返回值

---
+++ src/chapter21/CallableDemo.java
@@ -0,0 +1,66 @@
+package chapter21;
+
+import java.util.ArrayList;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+/**
+ * Created by shadowwingz on 2018-07-24 19:44
+ *
+ * Runnable 不返回值
+ * Callable 会返回值
+ */
+class TaskWithResult implements Callable<String> {
+
+    private int id;
+
+    public TaskWithResult(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String call() throws Exception {
+        return "result of TaskWithResult " + id;
+    }
+}
+
+public class CallableDemo {
+    public static void main(String[] args) {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        /**
+         * submit 方法会产生 Future 对象
+         */
+        ArrayList<Future<String>> results = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            results.add(exec.submit(new TaskWithResult(i)));
+        }
+        for (Future<String> fs : results) {
+            try {
+                System.out.println(fs.get());
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            } finally {
+                exec.shutdown();
+            }
+        }
+    }
+}
+
+/*
+输出：
+
+result of TaskWithResult 0
+result of TaskWithResult 1
+result of TaskWithResult 2
+result of TaskWithResult 3
+result of TaskWithResult 4
+result of TaskWithResult 5
+result of TaskWithResult 6
+result of TaskWithResult 7
+result of TaskWithResult 8
+result of TaskWithResult 9
+
+ */
