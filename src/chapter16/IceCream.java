63897372c70dd9e4c8c525524207f2065ba8ba43
shadowwingz <shadowwingzhang@gmail.com>
2018-08-14 21:03:34 +0800

    返回一个数组

---
+++ src/chapter16/IceCream.java
@@ -0,0 +1,55 @@
+package chapter16;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * Created by shadowwingz on 2018-08-14 20:49
+ * <p>
+ * 返回一个数组
+ */
+public class IceCream {
+
+    private static Random rand = new Random(47);
+
+    static final String[] FLAVORS = {
+            "Chocolate", "Strawberry", "Vanilla Fudge Swirl",
+            "Mint Chip", "Mocha Almond Fudge", "Rum Raisin",
+            "Praline Cream", "Mud Pie"
+    };
+
+    public static String[] flavorSet(int n) {
+        if (n > FLAVORS.length)
+            throw new IllegalArgumentException("Set too big");
+        String[] results = new String[n];
+        boolean[] picked = new boolean[FLAVORS.length];
+        for (int i = 0; i < n; i++) {
+            int t;
+            do {
+                t = rand.nextInt(FLAVORS.length);
+            } while (picked[t]);
+            results[i] = FLAVORS[t];
+            picked[t] = true;
+        }
+        return results;
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 7; i++) {
+            System.out.println(Arrays.toString(flavorSet(3)));
+        }
+    }
+}
+
+/*
+输出：
+
+[Rum Raisin, Mint Chip, Mocha Almond Fudge]
+[Chocolate, Strawberry, Mocha Almond Fudge]
+[Strawberry, Mint Chip, Mocha Almond Fudge]
+[Rum Raisin, Vanilla Fudge Swirl, Mud Pie]
+[Vanilla Fudge Swirl, Chocolate, Mocha Almond Fudge]
+[Praline Cream, Strawberry, Mocha Almond Fudge]
+[Mocha Almond Fudge, Strawberry, Mint Chip]
+
+ */
