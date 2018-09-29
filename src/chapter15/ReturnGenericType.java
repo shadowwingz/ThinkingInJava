0e2f8ac4e78298ebb5839fe147a2e42f2de0b81f
shadowwingz <shadowwingzhang@gmail.com>
2018-09-29 22:28:21 +0800

    擦除的神秘之处 补充

--- src/chapter15/ReturnGenericType.java
+++ src/chapter15/ReturnGenericType.java
@@ -2,6 +2,10 @@ package chapter15;
 
 /**
  * Created by shadowwingz on 2018-08-19 17:58
+ * <p>
+ * 当使用 ReturnGenericType 时，T 会被擦除为 HasF 或 HasF 的子类，
+ * 这个时候，T 的类型是不确定的，只能知道它的范围，即 HasF 或 HasF 的子类。
+ * 但是，当我们调用 getObj 方法时，会返回具体类型。
  */
 public class ReturnGenericType<T extends HasF> {
 
@@ -14,4 +18,20 @@ public class ReturnGenericType<T extends HasF> {
     public T getObj() {
         return obj;
     }
+
+    public static void main(String[] args) {
+        ReturnGenericType type = new ReturnGenericType(new A());
+        System.out.println(type.getObj().getClass().getSimpleName());
+    }
+
+    static class A extends HasF {
+
+    }
 }
+
+/*
+输出：
+
+A
+
+ */
