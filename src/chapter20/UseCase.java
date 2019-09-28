b850f6515c26aa0e2171343bf4500839efbde735
shadowwingz <shadowwingzhang@gmail.com>
2019-09-28 18:10:21 +0800

    编写注解处理器

--- src/chapter20/UseCase.java
+++ src/chapter20/UseCase.java
@@ -5,7 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+// @Target 用来定义你的注解将应用于什么地方，比如一个方法或者一个域
 @Target(ElementType.METHOD)
+// @Retention 用来定义该注解在哪一个级别可用，
+// 在源代码中(SOURCE)，类文件中（CLASS），或者运行时（RUNTIME）
 @Retention(RetentionPolicy.RUNTIME)
 public @interface UseCase {
     public int id();
---
+++ src/chapter20/UseCaseTracker.java
@@ -0,0 +1,32 @@
+package chapter20;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class UseCaseTracker {
+    private static void trackUseCases(List<Integer> useCases, Class<?> cl) {
+        // 获取 Class 中所有的方法
+        for (Method m : cl.getDeclaredMethods()) {
+            // 获取每一个方法的注解
+            // 如果方法有 UseCase 的注解，就返回 UseCase 类型的注解，
+            // 否则返回 null
+            UseCase uc = m.getAnnotation(UseCase.class);
+            if (uc != null) {
+                // 打印注解中的 id 和 description
+                System.out.println("Found Use Case: " + uc.id() + " " + uc.description());
+            }
+            useCases.remove(new Integer(uc.id()));
+        }
+        for (Integer i : useCases) {
+            System.out.println("Warning: Missing use case " + i);
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Integer> useCases = new ArrayList<>();
+        Collections.addAll(useCases, 47, 48, 49, 50);
+        trackUseCases(useCases, PasswordUtils.class);
+    }
+}
