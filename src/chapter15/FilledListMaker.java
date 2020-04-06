1f01557a16af16e79cdc21d59c042c6908e58f7d
shadowwingz <shadowwingzhang@gmail.com>
2020-04-06 22:27:19 +0800

    泛型的补充

--- src/chapter15/FilledListMaker.java
+++ src/chapter15/FilledListMaker.java
@@ -15,15 +15,16 @@ public class FilledListMaker<T> {
          * 这个 ArrayList，添加 T 类型的数据不会报错，
          * 添加别的类型的数据就会报错，虽然编译器也不知道 T 类型到底是什么类型。
          *
-         * 在本例中，泛型 T 是 String，但编译器并不知道 T 是 String，
-         * 但是 ArrayList 添加 String 类型的数据，编译器不会报错，
-         * 添加 int 类型的数据，编译器就会报错。
+         * 在本例中，虽然我们指定的泛型 T 是 String 类型，但是对编译器来说，它并不知道 T 是 String，
+         * 它只知道 ArrayList 可以添加 T 类型的数据，所以 result.add(t) 不会报错。
+         * 但是如果让 ArrayList 添加任意一种其他类型，比如 Integer，或者 String，就会报错。
+         * 换句话说，ArrayList 只认 T，别的都不认。
          */
         List<T> result = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             result.add(t);
             // result.add(1) 报错
-//            result.add(1);
+//            result.add("1"); 报错
         }
         return result;
     }
